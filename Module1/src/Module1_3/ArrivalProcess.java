08ed889684da086697bf96dcb84b70f9d99c0042
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2025-09-10 17:05:34 +0300

    Add ArrivalProcess for Mod 1.3 task 6

---
+++ Module1/src/Module1_3/ArrivalProcess.java
@@ -0,0 +1,43 @@
+package Module1_3;
+
+import java.util.List;
+import java.util.ArrayList;
+import eduni.distributions.Normal;
+import Module1_2.Event;
+import Module1_2.EventType;
+
+/*
+ *   Orientation tasks
+ *       Module 1.3, Task 6
+ */
+public class ArrivalProcess {
+    private Normal rng;
+    private EventType eventType;
+
+    public ArrivalProcess(EventType eventType, double mean) {
+        this.eventType = eventType;
+        this.rng = new Normal(mean, 10.0, 123); // standard deviation fixed at 10.0
+    }
+
+    public void addArrival(List<Event> eventList, double currentTime) {
+        double interval = rng.sample();
+        double eventTime = currentTime + interval;
+        eventList.add(new Event(this.eventType, (long)eventTime));
+    }
+
+    // Test program
+    public static void main(String[] args) {
+        List<Event> eventList = new ArrayList<>();
+        ArrivalProcess ap = new ArrivalProcess(EventType.ARRIVAL, 50.0);
+        double clock = 0.0;
+
+        for (int i = 0; i < 10; i++) {
+            ap.addArrival(eventList, clock);
+            clock = eventList.get(eventList.size() - 1).getTime(); // move clock to new event time
+        }
+
+        for (Event event : eventList) {
+            System.out.println(event);
+        }
+    }
+}
